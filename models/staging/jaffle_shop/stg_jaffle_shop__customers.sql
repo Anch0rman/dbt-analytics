d0b5a7593fdd3ff9034ce9b52cf10cc0e5e038f6
John Blankemeier <john@ecomedgetech.com>
2026-05-11 15:43:45 +0000

    Move to subfolder

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,6 @@
-    select
-        id as customer_id,
-        first_name,
-        last_name
+select
+    id as customer_id,
+    first_name,
+    last_name
 
-    from dbt-tutorial.jaffle_shop.customers
+from dbt-tutorial.jaffle_shop.customers
